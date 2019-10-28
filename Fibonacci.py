5b92c02c97bde13b17e978d2259d3da9d6da141f
Nitish-apex <nitishvelu@gmail.com>
2019-10-28 15:18:23 +0530

    fixed fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,18 +1,28 @@
-# Function for nth Fibonacci number 
-  
-def Fibonacci(n): 
-    if n<0: 
-        print("Incorrect input") 
-    # First Fibonacci number is 0 
-    elif n==1: 
-        return 0
-    # Second Fibonacci number is 1 
-    elif n==2: 
-        return 1
-    else: 
-        return Fibonacci(n-1)+Fibonacci(n-2) 
-  
-# Driver Program 
-  
-print(Fibonacci(9)) 
-  
+# Program to display the Fibonacci sequence up to n-th term where n is provided by the user
+
+
+nterms =int(input("Enter a Number :"))
+
+# uncomment to take input from the user
+#nterms = int(input("How many terms? "))
+
+# first two terms
+n1 = 0
+n2 = 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence upto",nterms,":")
+   while count < nterms:
+       print(n1,end=' \n ')
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
